24d92743c8b277b2b7a8592129ff86a5bf4e6307
pdaulby <p.daulby@hotmail.com>
2022-03-19 22:59:13 +0000

    add test for crime and postcode api errors

--- src/test/java/com/paul/postcode/crimeLocation/CrimeLocationControllerTest.java
+++ src/test/java/com/paul/postcode/crimeLocation/CrimeLocationControllerTest.java
@@ -6,7 +6,7 @@ import static com.shazam.shazamcrest.MatcherAssert.assertThat;
 import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.paul.postcode.crimeLocation.model.Crime;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +30,10 @@ class CrimeLocationControllerTest {
         wireMockServer = new WireMockServer(8089);
         wireMockServer.start();
     }
+    @AfterEach
+    void cleanup() {
+        wireMockServer.stop();
+    }
 
     @Test
     public void shouldReturnCrime_PostcodeAndCrimesAPI_working() throws Exception {
@@ -45,6 +49,37 @@ class CrimeLocationControllerTest {
 
         String expected = "{\"crimes\":[{\"category\":\"violent-crime\",\"street\":\"On or near Abbey Gate\",\"outcome\":\"Unable to prosecute suspect\",\"month\":\"2017-02\"}]}";
         String actual = this.restTemplate.getForObject("http://localhost:" + port + "/crimes-at-postcode/s71gq", String.class);
+
+        assertThat(actual, sameBeanAs(expected));
+    }
+
+    @Test
+    public void shouldReturnCrime_CrimesAPIFailing() throws Exception {
+        wireMockServer.stubFor(get("/postcodes/s71gq")
+                .willReturn(ok()
+                        .withStatus(200)
+                        .withBody("{\"status\": 200,\"result\": {\"longitude\": 1.1,\"latitude\": 50.1}}")));
+
+        wireMockServer.stubFor(get("/api/crimes-at-location?date=2017-02&lat=50.1&lng=1.1")
+                .willReturn(ok()
+                        .withStatus(500)
+                        .withBody("")));
+
+        String expected = "{\"errorMessage\":\"There was an issue with the Police Data API\"}";
+        String actual = this.restTemplate.getForObject("http://localhost:" + port + "/crimes-at-postcode/s71gq", String.class);
+
+        assertThat(actual, sameBeanAs(expected));
+    }
+
+    @Test
+    public void shouldReturnCrime_PostcodeAPINotFound() throws Exception {
+        wireMockServer.stubFor(get("/postcodes/s73gq")
+                .willReturn(ok()
+                        .withStatus(404)
+                        .withBody("{\"status\": 404,\"error\": \"Postcode not found\"}")));
+
+        String expected = "{\"errorMessage\":\"Postcode not Found\"}";
+        String actual = this.restTemplate.getForObject("http://localhost:" + port + "/crimes-at-postcode/s73gq", String.class);
 
         assertThat(actual, sameBeanAs(expected));
     }
